6d24c06344129b3911a0f090f730475f86b3b9ae
linnefromice <linnefromice@gmail.com>
2024-07-01 17:46:02 +0900

    update

--- code-jump.com/lp-intro-1/src/Contents/index.tsx
+++ code-jump.com/lp-intro-1/src/Contents/index.tsx
@@ -1,12 +1,16 @@
-import styles from './styles.module.css'
+import { ReactNode } from "react";
+import styles from "./styles.module.css";
 
-export const Contents = (props) => {
+type Props = {
+  title: string;
+  children?: ReactNode;
+};
+export const Contents: React.FC<Props> = (props) => {
   return (
-    <div className={styles.root}>
+    <div className={styles.wrapper}>
       <h2 className={styles.title}>{props.title}</h2>
       <div className={styles.spacer} />
       {props.children}
     </div>
-  )
-}
-
+  );
+};
--- code-jump.com/lp-intro-1/src/app/index.tsx
+++ code-jump.com/lp-intro-1/src/app/index.tsx
@@ -1,34 +1,20 @@
-import { Contents } from '../Contents'
-import { Header } from '../Header'
-import styles from './styles.module.css'
+import { About } from "../About";
+import { Contents } from "../Contents";
+import { Header } from "../Header";
+import styles from "./styles.module.css";
 
 function App() {
   return (
     <div className={styles.main}>
       <Header />
       <img src="/mainvisual-pc.jpg" alt="mainvisual" />
-      <Contents
-        title={`About`}
-      >
-        <div>Xxxxx Ashley</div>
-        <div>
-          2th Floor xxxxx Building x-x-x Nishiazabu, Minato-ku, Tokyo 106-0031 Japan
-          tel: 000-0000-0000
-          url: www.xxxxxx.jp
-          mail: xxx@xxxxxx.jp
-        </div>
-        <div>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-        </div>
+      <Contents title={`About`}>
+        <About />
       </Contents>
-      <Contents
-        title={`Works`}
-      />
-      <Contents
-        title={`News`}
-      />
+      <Contents title={`Works`} />
+      <Contents title={`News`} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
